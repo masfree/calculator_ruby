f3e475939f18afe53a64dc65077cc680e71a1fc6
masfree <alexander_m@appbooster.ru>
2018-04-30 13:08:15 +0400

    Added testing (#1)

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,55 +1,40 @@
 class Calculator
-  def initialize number1, number2, operation
-    @number1 = number1.to_f
-    @number2 = number2.to_f
+  attr_accessor :first_operator, :second_operator
+  attr_writer :operation
+
+  OPERATIONS = ['+', '-', '*', '/']
+
+  def initialize first_operator = nil, second_operator = nil, operation = nil
+    @first_operator = first_operator
+    @second_operator = second_operator
     @operation = operation
   end
 
-  # Метод определяет есть ли числа после запятой так же относится ли к бескончности.
-  def have_an_remainder? number
-    # почему-то условие "или" покрывает все значение
-    # от минус бесконечности до плюс,
-    # потому условие пришлось разделить
-    return true if number == Float::INFINITY
-    return true if number ==  -Float::INFINITY
-
-    if number.to_i != 0
-      number.to_f % number.to_i != 0
-    else
-      true
-    end
-  end
-
-  def sum
-    @number1 + @number2
-  end
-
-  def subtraction
-    @number1 - @number2
-  end
-
-  def multiplication
-    @number1 * @number2
-  end
-
-  def division
-    @number1 / @number2
-  end
-
-  def operation
-    case @operation
-      when '+' then sum
-      when '-' then subtraction
-      when '*' then multiplication
-      when '/' then division
-    end
+  def to_flat
+    first_operator.to_f
   end
 
   def answer
-    if have_an_remainder? operation
-      operation.to_f
-    else
-      operation.to_i
+    calculation = calculate
+
+    begin
+      calculation = calculation.to_i if calculation.to_i == calculation
+    ensure
+      return calculation
     end
+  end
+
+  private
+
+  def calculate
+    if filter_operation
+      eval "#{first_operator.to_f} #{@operation} #{second_operator.to_f}"
+    else
+      raise "Wrong operator please use #{OPERATIONS.join(", ")} operators"
+    end
+  end
+
+  def filter_operation
+    OPERATIONS.include? @operation
   end
 end
